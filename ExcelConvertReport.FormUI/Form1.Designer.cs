cff235f51ecfc79cd73291f4836ae9e1155f3026
HakanŞahin <abc@a.a>
2022-10-09 19:15:59 +0300

    Test

--- ExcelConvertReport.FormUI/Form1.Designer.cs
+++ ExcelConvertReport.FormUI/Form1.Designer.cs
@@ -29,18 +29,19 @@ namespace ExcelConvertReport.FormUI
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dgwScreen = new System.Windows.Forms.DataGridView();
             this.btnLoad = new System.Windows.Forms.Button();
             this.btnConvert = new System.Windows.Forms.Button();
             this.grb = new System.Windows.Forms.GroupBox();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnSearch = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpFinish = new System.Windows.Forms.DateTimePicker();
             this.ofd = new System.Windows.Forms.OpenFileDialog();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgwScreen)).BeginInit();
             this.grb.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -49,119 +50,143 @@ namespace ExcelConvertReport.FormUI
             // dgwScreen
             // 
             this.dgwScreen.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgwScreen.BackgroundColor = System.Drawing.Color.White;
+            this.dgwScreen.BackgroundColor = System.Drawing.Color.Linen;
             this.dgwScreen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dgwScreen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Info;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgwScreen.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dgwScreen.Location = new System.Drawing.Point(12, 12);
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgwScreen.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgwScreen.Location = new System.Drawing.Point(12, 13);
             this.dgwScreen.Name = "dgwScreen";
-            this.dgwScreen.Size = new System.Drawing.Size(776, 361);
+            this.dgwScreen.Size = new System.Drawing.Size(776, 389);
             this.dgwScreen.TabIndex = 0;
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(165, 38);
+            this.btnLoad.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnLoad.Location = new System.Drawing.Point(165, 19);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(108, 29);
+            this.btnLoad.Size = new System.Drawing.Size(108, 31);
             this.btnLoad.TabIndex = 1;
             this.btnLoad.Text = "Yükle";
             this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.UseWaitCursor = true;
             this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
             // btnConvert
             // 
-            this.btnConvert.Location = new System.Drawing.Point(165, 73);
+            this.btnConvert.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnConvert.Location = new System.Drawing.Point(165, 57);
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(108, 29);
+            this.btnConvert.Size = new System.Drawing.Size(108, 31);
             this.btnConvert.TabIndex = 2;
             this.btnConvert.Text = "Dönüştür";
             this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.UseWaitCursor = true;
             this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
             // grb
             // 
+            this.grb.Controls.Add(this.btnTransfer);
             this.grb.Controls.Add(this.pictureBox1);
             this.grb.Controls.Add(this.btnSearch);
             this.grb.Controls.Add(this.label1);
-            this.grb.Controls.Add(this.dateTimePicker2);
-            this.grb.Controls.Add(this.dateTimePicker1);
+            this.grb.Controls.Add(this.dtpStart);
+            this.grb.Controls.Add(this.dtpFinish);
             this.grb.Controls.Add(this.btnConvert);
             this.grb.Controls.Add(this.btnLoad);
-            this.grb.Location = new System.Drawing.Point(794, 12);
+            this.grb.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grb.Location = new System.Drawing.Point(794, 13);
             this.grb.Name = "grb";
-            this.grb.Size = new System.Drawing.Size(408, 361);
+            this.grb.Size = new System.Drawing.Size(408, 389);
             this.grb.TabIndex = 3;
             this.grb.TabStop = false;
             this.grb.Tag = "";
             this.grb.Text = "İşlemler";
+            this.grb.Enter += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(165, 94);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(108, 31);
+            this.btnTransfer.TabIndex = 8;
+            this.btnTransfer.Text = "Excel \'e Aktar";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(60, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 131);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(348, 165);
+            this.btnSearch.Location = new System.Drawing.Point(348, 178);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(54, 20);
             this.btnSearch.TabIndex = 6;
             this.btnSearch.Text = "Ara";
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.UseWaitCursor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(162, 133);
+            this.label1.Location = new System.Drawing.Point(158, 143);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.Size = new System.Drawing.Size(127, 14);
             this.label1.TabIndex = 5;
             this.label1.Text = "Tarih Aralığına Göre Getir";
+            this.label1.UseWaitCursor = true;
             // 
-            // dateTimePicker2
+            // dtpStart
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(6, 165);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(165, 20);
-            this.dateTimePicker2.TabIndex = 4;
+            this.dtpStart.Location = new System.Drawing.Point(6, 178);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(165, 20);
+            this.dtpStart.TabIndex = 4;
             // 
-            // dateTimePicker1
+            // dtpFinish
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(177, 165);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(165, 20);
-            this.dateTimePicker1.TabIndex = 3;
+            this.dtpFinish.Location = new System.Drawing.Point(177, 178);
+            this.dtpFinish.Name = "dtpFinish";
+            this.dtpFinish.Size = new System.Drawing.Size(165, 20);
+            this.dtpFinish.TabIndex = 3;
+            this.dtpFinish.UseWaitCursor = true;
             // 
             // ofd
             // 
             this.ofd.FileName = "openFileDialog1";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(60, 217);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(299, 122);
-            this.pictureBox1.TabIndex = 7;
-            this.pictureBox1.TabStop = false;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.Lime;
-            this.ClientSize = new System.Drawing.Size(1219, 385);
+            this.ClientSize = new System.Drawing.Size(1206, 415);
             this.Controls.Add(this.grb);
             this.Controls.Add(this.dgwScreen);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Salman Mühendislik Serbest Rapor Düzenleme";
+            this.Tag = "";
+            this.Text = "SERBEST RAPOR DUZENLEME";
             ((System.ComponentModel.ISupportInitialize)(this.dgwScreen)).EndInit();
             this.grb.ResumeLayout(false);
             this.grb.PerformLayout();
@@ -178,10 +203,11 @@ namespace ExcelConvertReport.FormUI
         private System.Windows.Forms.GroupBox grb;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpFinish;
         private System.Windows.Forms.OpenFileDialog ofd;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnTransfer;
     }
 }
 
--- ExcelConvertReport.FormUI/Form1.cs
+++ ExcelConvertReport.FormUI/Form1.cs
@@ -5,10 +5,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using Microsoft.Office.Interop.Excel;
+//using excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelConvertReport.FormUI
 {
@@ -18,6 +21,11 @@ namespace ExcelConvertReport.FormUI
         public Form1()
         {
             InitializeComponent();
+            btnSearch.Enabled = false;
+            dtpStart.Enabled = false;
+            dtpFinish.Enabled = false;
+            btnConvert.Enabled = false;
+            btnTransfer.Enabled = false;
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
@@ -30,57 +38,132 @@ namespace ExcelConvertReport.FormUI
                 OleDbConnection connect = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source = "
                     + path + "; Extended Properties = Excel 12.0");
                 connect.Open();
-                string query = "SELECT * FROM [Sayfa1$A:B] ";
+                string query = "SELECT * FROM [Sayfa1$] ";
                 OleDbDataAdapter data_adaptor = new OleDbDataAdapter(query, connect);
                 connect.Close();
 
                 DataTable dt = new DataTable();
                 data_adaptor.Fill(dt);
                 dgwScreen.DataSource = dt;
+                btnConvert.Enabled = true;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                MessageBox.Show($"Bir hata oluştu! \n Hata: {ex}");
+                MessageBox.Show($"Seçilen dosya uygun formatta değil!");
             }
-
         }
-
         public static List<Report> list = new List<Report>();
+        int columCount = 0;
+
         private void btnConvert_Click(object sender, EventArgs e)
         {
-            int columCount = dgwScreen.Columns.Count;
             try
             {
+                columCount = dgwScreen.Columns.Count;
+            
                 for (int j = 0; j < dgwScreen.Rows.Count; j++)
                 {
                     for (int i = 0; i < columCount; i++)
                     {
-                        report.CustomerCode = dgwScreen.Rows[j].Cells[i].Value.ToString();
-                        report.CustomerName = dgwScreen.Rows[j].Cells[i + 1].Value.ToString();
-                        report.OrderNumber = dgwScreen.Rows[j].Cells[i + 2].Value.ToString();
-                        report.OrderDate = DateTime.Parse(dgwScreen.Rows[j].Cells[i + 3].Value.ToString());
-                        report.DeadLine = DateTime.Parse(dgwScreen.Rows[j].Cells[i + 4].Value.ToString());
-                        report.DeadLineWeek = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 5].Value);
-                        report.ProjectCode = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 6].Value.ToString());
-                        report.OrderDesk = Convert.ToInt32(dgwScreen.Rows[j + 7].Cells[i].Value);
-                        report.StockCode = dgwScreen.Rows[j].Cells[i + 8].Value.ToString();
-                        report.StockName = dgwScreen.Rows[j].Cells[i + 9].Value.ToString();
-                        report.Unit = dgwScreen.Rows[j].Cells[i + 10].Value.ToString();
-                        report.OrderQuantity = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 11].Value.ToString());
-                        report.DeliveryAmount = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 12].Value.ToString());
-                        report.BalanceAmount = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 13].Value.ToString());
-                    }
-                    if (report.CustomerName != null)
-                    {
-                        list.Add(report);
+                        if (dgwScreen.Rows[j].Cells[i + 1].Value != null)
+                        {
+                            report.CustomerCode = dgwScreen.Rows[j].Cells[i].Value.ToString();
+                            report.CustomerName = dgwScreen.Rows[j].Cells[i + 1].Value.ToString();
+                            report.OrderNumber = dgwScreen.Rows[j].Cells[i + 2].Value.ToString();
+                            report.OrderDate = DateTime.ParseExact(dgwScreen.Rows[j].Cells[i + 3].Value.ToString(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                            report.DeadLine = DateTime.ParseExact(dgwScreen.Rows[j].Cells[i + 4].Value.ToString(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                            report.DeadLineWeek = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 5].Value);
+                            report.ProjectCode = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 6].Value.ToString());
+                            report.OrderDesk = Convert.ToInt32(dgwScreen.Rows[j + 7].Cells[i].Value);
+                            report.StockCode = dgwScreen.Rows[j].Cells[i + 8].Value.ToString();
+                            report.StockName = dgwScreen.Rows[j].Cells[i + 9].Value.ToString();
+                            report.Unit = dgwScreen.Rows[j].Cells[i + 10].Value.ToString();
+                            report.OrderQuantity = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 11].Value.ToString());
+                            report.DeliveryAmount = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 12].Value.ToString());
+                            report.BalanceAmount = Convert.ToInt32(dgwScreen.Rows[j].Cells[i + 13].Value.ToString());
+                            if (report.OrderDate == list[j-1].OrderDate && report.CustomerName == list[j-1].CustomerName/* && report.StockName == list[j - 1].StockName*/)
+                            {
+                                list[j - 1].OrderQuantity += report.OrderQuantity.Value;
+                            }
+                            else
+                            list.Add(report);
+                        }
                     }
                 }
-                dgwScreen.DataSource = list;
+                dgwScreen.DataSource = (list.Select(x => new
+                {
+                    x.CustomerCode,
+                    x.CustomerName,
+                    x.OrderNumber,
+                    x.OrderDate,
+                    x.DeadLine,
+                    x.DeadLineWeek,
+                    x.StockCode,
+                    x.StockName,
+                    x.Unit,
+                    x.OrderQuantity,
+                    x.DeliveryAmount,
+                    x.BalanceAmount
+                }))
+                .OrderBy(x => x.CustomerName);
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Bir hata oluştu! \n Hata : {ex}");
             }
         }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    excel.Application app = new excel.Application();
+            //    app.Visible = true;
+            //    Workbook book = app.Workbooks.Add(System.Reflection.Missing.Value);
+            //    Worksheet sheet = (Worksheet)book.Sheets[1];
+            //    for (int i = 0; i < dgwScreen.Columns.Count; i++)
+            //    {
+            //        Range range = (Range)sheet.Cells[1, 1];
+            //        range.Cells[1, i + 1] = dgwScreen.Columns[i].HeaderText;
+            //    }
+            //    for (int i = 0; i < columCount; i++)
+            //    {
+            //        for (int j = 0; j < dgwScreen.Rows.Count; j++)
+            //        {
+            //            Range range = (Range)sheet.Cells[j + 1, i + 1];
+            //            range.Cells[2, 1] = dgwScreen[i, j].Value;
+            //        }
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("Lütfen seçili dosyanın doğru formatta olduğunu kontrol ediniz!");
+            //}
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            DateTime start = dtpStart.Value;
+            DateTime finish = dtpFinish.Value;
+
+            var complexType = (list.Select(x => new
+            {
+                x.CustomerCode,
+                x.CustomerName,
+                x.OrderNumber,
+                x.OrderDate,
+                x.DeadLine,
+                x.DeadLineWeek,
+                x.StockCode,
+                x.StockName,
+                x.Unit,
+                x.OrderQuantity,
+                x.DeliveryAmount,
+                x.BalanceAmount
+            }))
+            .Where(x => x.OrderDate >= start && x.OrderDate <= finish)
+            .OrderBy(x => x.CustomerName);
+            dgwScreen.DataSource = complexType;
+        }
+     
     }
 }
